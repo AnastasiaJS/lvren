6e41409e4faab3f7a2f9e460e3180d87f8985603
AnastasiaJS <245915765@qq.com>
2017-03-03 18:11:27 +0800

    [Add]cardmore

--- routes/card.js
+++ routes/card.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('cardDetail');
 });
+router.get('/more', function(req, res, next) {
+  res.render('moreCards');
+});
 
 module.exports = router;
